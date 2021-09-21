a519ae69cf9f42d1549c589dde003969dd617a10
Student <student@Cumberlandss-MacBook-Pro.local>
2021-09-20 21:39:05 -0500

    Completed the JSBasicsExercise and the Bonus.

---
+++ script.js
@@ -0,0 +1,63 @@
+// #1
+console.log('13 is my favorite #');
+
+// #2
+// Numbers,
+// Strings,
+// Booleans,
+// Null,
+// Undefined,
+
+// #3
+// Adding +
+// Subtracting -
+// Multiplying *
+// Dividing /
+// Exponentiation/exponents **
+// Modulus %
+
+// #4
+console.log(5%4);
+console.log(1%4);
+
+// #5
+console.log('I\'M MARY POPPINS, Y\'ALL');
+
+// #6 
+console.log("HAWK" + "eye");
+console.log("HAWK" +"" +"eye");
+console.log("HAWK " + "eye");
+
+// 7
+console.log("SJS".length);
+console.log("Sonya Silverman".length);
+console.log("Sonya".length + "Silverman".length);
+
+// #8
+console.log("Avengers:Endgame"[15]);
+
+// Bonus
+// #1
+console.log("\\");
+
+// #2
+console.log(5**4-9);
+
+// #3
+console.log("I".length - "Am".length + "Iron".length % "Man".length);
+
+// #4
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+// #5
+console.log("var m=25");
+console.log("var c=4");
+console.log("var u= m + c");
+console.log("u");
